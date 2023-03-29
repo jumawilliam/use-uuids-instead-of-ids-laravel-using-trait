890a68b968bb87d0564f18c6ebd4fb74609cdb53
william <jumawilliam2011@gmail.com>
2023-03-30 00:15:10 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\User;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    //return User::find('98ce565f-b311-4a4a-a81e-19bd81a6c5b8');
+    return view('welcome');
+});
